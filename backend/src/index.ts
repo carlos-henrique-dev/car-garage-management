ea8b0412f517caef89a4423ecff7812b8a610927
Carlos Henrique <henriqueok20@gmail.com>
2022-09-09 23:51:43 -0400

    initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import express, { Express, Request, Response } from 'express'
+
+const app: Express = express()
+const port = process.env.PORT
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
